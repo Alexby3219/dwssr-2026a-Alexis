a43f0857646047786227df12d977ac7ea3de6d80
Alexby3219 <alexisgabriel.mora32@gmail.com>
2026-03-11 16:56:10 +0000

    👌Feat: Add author route and view, update index page, and enhance logging

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+// importar el router del autor
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authorRouter = require('./routes/author');
 
 var app = express();
 
@@ -18,9 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+console.logp(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/author', authorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

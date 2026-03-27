fe77629ee01ecf2be253b9c42e11d3375e87a789
Alexby3219 <alexisgabriel.mora32@gmail.com>
2026-03-27 05:28:31 +0000

    chore: update package.json to include vite and fix formatting; add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// importo la funcion defineConfig de Vite
+import { defineConfig } from 'vite'
+// imoporto un resolvedor de rutas
+import { resolve } from "node:path"
+
+// exporto la configuracion de Vite
+export default defineConfig({})
+  // configuro el servidor de desarrollo

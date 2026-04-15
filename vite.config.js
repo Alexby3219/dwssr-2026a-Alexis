a5109c6b73b590ea0bd15209cd71498267c0bb63
Alexby3219 <alexisgabriel.mora32@gmail.com>
2026-04-15 16:12:45 +0000

    Commite de Vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,27 @@ import { defineConfig } from 'vite'
 import { resolve } from "node:path"
 
 // exporto la configuracion de Vite
-export default defineConfig({})
-  // configuro el servidor de desarrollo
+export default defineConfig({
+  // Directorio raiz de los archivos fuente
+  root: 'src',
+
+  // Configuracion del servidor de desarrollo de front-end
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+
+  // Configuracion del build
+  build: {
+    // Directorio de salida
+    outDir: './dist',
+    emptyOutDir: true,
+    // Generar un manifiesto
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/main.js')
+      }
+    }
+  }
+})
